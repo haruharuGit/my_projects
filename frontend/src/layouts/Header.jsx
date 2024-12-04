e3791f5e2d7da47e3979288c1052b029a4feb7f5
haruharuGit <chiharu.fu.54@gmail.com>
2024-12-04 22:07:11 +0900

    ヘッダーのロゴに投稿一覧へのリンクを追加

--- frontend/src/layouts/Header.jsx
+++ frontend/src/layouts/Header.jsx
@@ -1,10 +1,13 @@
 import { Box, Flex, Heading, Button } from '@chakra-ui/react'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <Box bg='#FEBE6A' py={4} px={6}>
       <Flex justify='space-between' align='center'>
-        <Heading size='lg'>リアルTWO</Heading>
+        <Link to="/">
+          <Heading size='lg' as='h1'>リアルTWO</Heading>
+        </Link>
         <Button variant='ghost'>ログアウト</Button>
       </Flex>
     </Box>
