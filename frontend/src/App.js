1f00e36d97048ad5aa444a92dc61927c0cf08870
haruharuGit <chiharu.fu.54@gmail.com>
2024-12-05 23:59:24 +0900

    Reactでユーザー新規登録画面のルーティングを作成

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Posts from './pages/Posts';
 import CreatePost from './pages/CreatePost';
+import SignUp from './pages/SignUp';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Posts />} />
       <Route path="/create" element={<CreatePost />} />
+      <Route path="/signup" element={<SignUp />} />
     </Routes>
   );
 }
