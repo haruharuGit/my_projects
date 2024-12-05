6fb3a52e1c65f041d9f07dadc62ddaf53596d8c0
haruharuGit <chiharu.fu.54@gmail.com>
2024-12-05 16:09:14 +0900

    sign_upのルーティングを設定

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :posts
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+      }
     end
   end
 end
