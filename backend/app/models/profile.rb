06391162c262859fb0df594563c1312c7371da52
haruharuGit <chiharu.fu.54@gmail.com>
2024-12-09 09:12:44 +0900

    プロフィールモデルにバリデーションとアソシエーションを追加

--- backend/app/models/profile.rb
+++ backend/app/models/profile.rb
@@ -1,2 +1,8 @@
 class Profile < ApplicationRecord
+  belongs_to :user
+
+  with_options presence: true do
+    validates :nickname, length: { maximum: 10 }
+    validates :kid_birthday, numericality
+  end
 end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_05_061005) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_08_235213) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -45,6 +45,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_05_061005) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "profiles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "kid_birthday", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
@@ -72,4 +81,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_05_061005) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "profiles", "users"
 end
