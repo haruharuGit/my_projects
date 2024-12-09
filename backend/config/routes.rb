b82b9a05cdad962fa607557e6d0cc88cb8f87d7c
haruharuGit <chiharu.fu.54@gmail.com>
2024-12-09 18:03:17 +0900

    ルーティングを修正

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :posts
-      resources :users, only: [] do
-        resources :profiles, only: [:create, :update, :destroy, :show]
-      end
+      resources :profiles, only: [:create, :update, :destroy, :show]
+      get 'users/check_user', to: 'users#check_user'
 
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
         registrations: 'api/v1/auth/registrations',
