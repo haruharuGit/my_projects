8b5f64e49419f5f7a14d91fce43c871b0d85ccf1
haruharuGit <chiharu.fu.54@gmail.com>
2024-12-09 11:39:18 +0900

    profilesコントローラーのルーティングを追加

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :posts
+      resources :users, only: [] do
+        resources :profiles, only: [:create, :update, :destroy, :show]
+      end
 
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
         registrations: 'api/v1/auth/registrations',
-        sessions: 'api/v1/auth/sessions',
+        sessions: 'api/v1/auth/sessions'
       }
     end
   end
