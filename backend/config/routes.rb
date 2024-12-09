247c9c9b148e653c9c28085148c091000deb3c0e
haruharuGit <chiharu.fu.54@gmail.com>
2024-12-09 21:04:10 +0900

    新規ユーザー登録作成し直し

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,11 +3,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :posts
       resources :profiles, only: [:create, :update, :destroy, :show]
-      get 'users/check_user', to: 'users#check_user'
 
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
         registrations: 'api/v1/auth/registrations',
-        sessions: 'api/v1/auth/sessions'
       }
     end
   end
