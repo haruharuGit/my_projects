a2b4ec21b4666745eacadbbe45c3155d911bb487
haruharuGit <chiharu.fu.54@gmail.com>
2024-12-09 21:04:00 +0900

    ルーティングをApp.jsに変更

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Posts from './pages/Posts';
 import CreatePost from './pages/CreatePost';
 import SignUp from './pages/SignUp';
@@ -8,13 +8,15 @@ import CreateProfile from './pages/CreateProfile';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Posts />} />
-      <Route path="/create" element={<CreatePost />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/profile/create" element={<CreateProfile />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Posts />} />
+        <Route path="/create" element={<CreatePost />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/profile/create" element={<CreateProfile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
