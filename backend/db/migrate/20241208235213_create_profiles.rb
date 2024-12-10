5f7cae9ccfbd83ee3d6a361842fd15a4c761d0ca
haruharuGit <chiharu.fu.54@gmail.com>
2024-12-10 13:07:27 +0900

    kid_birthdayカラムをdate型に変更

--- backend/db/migrate/20241208235213_create_profiles.rb
+++ backend/db/migrate/20241208235213_create_profiles.rb
@@ -2,7 +2,7 @@ class CreateProfiles < ActiveRecord::Migration[7.0]
   def change
     create_table :profiles do |t|
       t.string :nickname, null: false
-      t.integer :kid_birthday, null: false
+      t.date :kid_birthday, null: false
       t.references :user, null: false, foreign_key: true
       t.timestamps
     end
