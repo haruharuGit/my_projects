461e20f5552130d5f7984080b46a4fd0949904bb
haruharuGit <chiharu.fu.54@gmail.com>
2024-12-11 17:17:36 +0900

    DBリセット

--- backend/bin/render-build.sh
+++ backend/bin/render-build.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+!/usr/bin/env bash
 # exit on error
 set -o errexit
 
@@ -11,3 +11,4 @@ bundle exec rails assets:clean
 # Uncomment the following line:
 
 # bundle exec rails db:migrate
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
