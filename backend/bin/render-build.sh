c24dee6eda06ab3702c4d37f80c85fc84608c520
haruharuGit <chiharu.fu.54@gmail.com>
2024-12-11 17:56:31 +0900

    buildファイル戻す

--- backend/bin/render-build.sh
+++ backend/bin/render-build.sh
@@ -1,33 +1,13 @@
-#!/usr/bin/env bash
-# Exit on error
-set -o errexit
-
-# Install required gems
-bundle install --jobs=4 --retry=3
-
-# Precompile assets for production
-export RAILS_ENV=production
-bundle exec rake assets:precompile
-
-# Clean old assets
-bundle exec rake assets:clean
-
-# Perform database migrations if using a Free instance type
-# Uncomment the following line if needed:
-# bundle exec rails db:migrate
-
-
-# 以下はデフォルト
 # !/usr/bin/env bash
 # exit on error
-# set -o errexit
+set -o errexit
 
-# bundle install
-# bundle exec rails assets:precompile
-# bundle exec rails assets:clean
+bundle install
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
 
 # If you're using a Free instance type, you need to
 # perform database migrations in the build command.
 # Uncomment the following line:
 
-# bundle exec rails db:migrate
+bundle exec rails db:migrate
