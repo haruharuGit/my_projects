f0078a0a2f8d20e102d38bd04ba66d6891009743
haruharuGit <chiharu.fu.54@gmail.com>
2024-12-17 14:19:19 +0900

    usersコントローラーのshowアクションを追加

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,13 @@
 class Api::V1::UsersController < ApplicationController
   # before_action :authenticate_api_v1_user!
+
+  def show
+    user = User.find(params[:id])
+    profile = user.profile
+    posts = user.posts
+    render json: { profile: profile, posts: posts }
+  end
+
   # 現在のユーザー情報を取得する
   def check_user_id
     if current_api_v1_user
