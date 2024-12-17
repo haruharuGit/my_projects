cace6cf35ee6aa5a669347b1c2fdc59020f8515f
haruharuGit <chiharu.fu.54@gmail.com>
2024-12-17 14:20:15 +0900

    ユーザー認証を行い、userIdを返す処理をAuth.jsxに移行

--- frontend/src/api/auth.jsx
+++ frontend/src/api/auth.jsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useState, useEffect } from "react";
 
 // 新規登録：未使用：後ほど移植してくる
 export const registerUser = async (email, password, passwordConfirmation) => {
@@ -49,4 +50,55 @@ export const logoutUser = async () => {
     console.error("Logout error:", error);
     throw error;
   }
+};
+
+// ユーザー認証し、user_id取得
+export const useAuthUserId = () => {
+  const [userId, setUserId] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+ 
+  useEffect(() => {
+    fetchGetUserId();
+  }, []);
+
+  const fetchGetUserId = async () => {
+    const accessToken = localStorage.getItem("access-token");
+    const client = localStorage.getItem("client");
+    const uid = localStorage.getItem("uid");
+
+    if (!accessToken || !client || !uid) {
+      console.error("認証情報が見つかりません");
+      alert("認証情報が見つかりません。ログインし直してください。");
+      setError("Authentication token is missing");
+      setIsLoading(false);
+      return;
+    }
+
+    try {
+      const res = await axios.get(
+        `${process.env.REACT_APP_BASE_URL}/api/v1/users/check_user_id`,
+        {
+          headers: {
+            "access-token": accessToken,
+            client: client,
+            uid: uid,
+          },
+        }
+      );
+
+      if (res.status >= 200 && res.status < 300) {
+        setUserId(res.data.id);
+      } else {
+        throw new Error(`HTTP error! status: ${res.status}`);
+      }
+    } catch (error) {
+      console.error("ユーザーIDの取得に失敗しました:", error);
+      setError("Failed to fetch user ID");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return { userId, isLoading, error };
 };
