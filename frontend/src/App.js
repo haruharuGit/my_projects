5582f2230179b5f575c9e65044cdf83bd1f02bf6
haruharuGit <chiharu.fu.54@gmail.com>
2024-12-17 14:20:32 +0900

    MyPageのReact側ルーティング設定

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,19 +6,19 @@ import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 import NewProfile from './pages/CreateProfile';
 import Home from './pages/Home'
-// import MyPage from './pages/MyPage'
+import MyPage from './pages/MyPage'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/index" element={<Posts />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/index" element={<Posts />} />
         <Route path="/create" element={<CreatePost />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/profile/create" element={<NewProfile />} />
-        {/* <Route path="/profile/:id" element={<MyPage />} /> */}
+        <Route path="/user/:id" element={<MyPage />} />
       </Routes>
     </BrowserRouter>
   );
